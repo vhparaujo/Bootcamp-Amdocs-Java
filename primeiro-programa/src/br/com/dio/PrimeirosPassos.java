32428a418a36be8d214ffc44a8bf1f45f55b0f68
vhparaujo <vhugopacheco59@gmail.com>
2022-01-17 11:52:47 -0300

    Primeiro commit bootcamp

---
+++ primeiro-programa/src/br/com/dio/PrimeirosPassos.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+import br.com.dio.model.Gato;
+
+public class PrimeirosPassos {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
